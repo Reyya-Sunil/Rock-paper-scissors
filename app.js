3909874a3b24f2de8f1090d45e4b937f24cf18d0
Sunil76-sudo <reyyasunil86@gmail.com>
2025-09-30 09:11:56 +0530

    Initial commit of Rock Paper Scissors game

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const choices = document.querySelectorAll(".choice");
 const msg = document.querySelector("#msg");
 const userScorePara =document.querySelector("#user-score");
 const compScorechoice =document.querySelector("#computer-score");
+// this the winner function if the winner win or draw ,lose then in msg ,box comes output
 
 const showWinner = (userwin,userChoice,ComputerChoice) => {
     if(userwin){
@@ -23,13 +24,17 @@ const showWinner = (userwin,userChoice,ComputerChoice) => {
     }
 }
 
-
+/*function related to the  generated the random number and choose the
+computer genrated option by array */
 
 const genComputerChoice = () =>{
 const option =["rock","paper","scissors"];
  const randIndx = Math.floor(Math.random()*3); //to created random number
  return option[randIndx];
 };
+
+/* if the game was draw then this function will be
+work that it prints the dialog box game was draw */
 
 const drawgame = () =>{
     console.log("game was draw")
@@ -38,12 +43,12 @@ const drawgame = () =>{
 }
 const playgame = (userChoice) =>{
     console.log("user choice :",userChoice)
-    // generated computer choice
+    // generated computer choice 
  const ComputerChoice =genComputerChoice();
  console.log("comp choice =",ComputerChoice)
+ //user choice part where user can select paper,scissors or rock
  if(userChoice===ComputerChoice){
-    //draw game
-    drawgame();
+ drawgame();//calling function
  }else{
     let userwin =true;
     if(userChoice ==="rock"){
@@ -53,9 +58,12 @@ const playgame = (userChoice) =>{
     }else if(userChoice=="scissors"){
         userwin=ComputerChoice==="rock"?false:true;
     }
-    showWinner(userwin,userChoice,ComputerChoice);
+    showWinner(userwin,userChoice,ComputerChoice); //calling function
  }
-};
+}; 
+/*function was clicked the image by user
+it tell the what image was clicked by the user it tell */
+
 choices.forEach((choice) =>{
     console.log(choice);
     choice.addEventListener("click",() =>{
